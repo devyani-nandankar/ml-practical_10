65072bb25df3e9733444362da5c3b622a561db37
Devyani Nandankar <devyaninandankar1@gmail.com>
2026-03-25 22:56:34 +0530

    first commit

---
+++ train_model.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+# Load dataset
+data = load_iris()
+X = data.data
+y = data.target
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Save model
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("Model trained and saved!")
